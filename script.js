437cf596c1bc85b028f40bb36022e4db2384bb11
Daniil Panov <from4life@pm.me>
2023-07-02 14:26:54 +0700

    Create insert function to insert new value to the tree

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class Tree {
         } else {
             arr.sort((a,b) => a - b);
             arr = [...new Set(arr)];
-            console.log(arr);
+            // console.log(arr);
        
             let mid = Math.floor(arr.length / 2);
             let leftArr = arr.slice(0, mid);
@@ -25,6 +25,32 @@ class Tree {
             let node = new Node(arr[mid], this.buildTree(leftArr), this.buildTree(rightArr));
             return node;
         }
+    }
+    insert(data) {
+        let node = this.root;
+        if (node === null) {
+            return node = new Node(data);
+        }
+        while(node !== null) {
+            if(node.data < data) {
+                if(node.right === null) {
+                    return node.right = new Node(data);
+                } else {
+                    node = node.right;
+                }
+                // node.right === null ? node.right = new Node(data) : node = node.right;
+            } else {
+                if(node.left === null) {
+                    return node.left = new Node(data);
+                } else {
+                    node = node.left;
+                }
+                // node.left === null ? node.left = new Node(data) : node = node.left;
+            }
+        }
+        // node = new Node(data);
+        // console.log(node)
+        // return;
     }
 }
 
@@ -44,5 +70,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 let newTree = new Tree(array);
+newTree.insert(322);
+
 prettyPrint(newTree.root);
 console.log(newTree.root);
