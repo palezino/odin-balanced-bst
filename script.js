da64ecca298dea15c601aa788a9ea6d5d55efe29
Daniil Panov <from4life@pm.me>
2023-07-03 19:22:18 +0700

    Create a delete function to remove elements from the tree

--- script.js
+++ script.js
@@ -1,76 +1,104 @@
 class Node {
-    constructor(data, left=null, right=null) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Tree {
-    constructor(arr) {
-        this.root = this.buildTree(arr);
-    }
-    buildTree(arr) {
-        // console.log(arr)
-        if (arr.length === 0) {
-            return null;
-        } else {
-            arr.sort((a,b) => a - b);
-            arr = [...new Set(arr)];
-            // console.log(arr);
-       
-            let mid = Math.floor(arr.length / 2);
-            let leftArr = arr.slice(0, mid);
-            let rightArr = arr.slice(mid + 1);
-            let node = new Node(arr[mid], this.buildTree(leftArr), this.buildTree(rightArr));
-            return node;
-        }
-    }
-    insert(data) {
-        let node = this.root;
-        if (node === null) {
-            return node = new Node(data);
-        }
-        while(node !== null) {
-            if(node.data < data) {
-                if(node.right === null) {
-                    return node.right = new Node(data);
-                } else {
-                    node = node.right;
-                }
-                // node.right === null ? node.right = new Node(data) : node = node.right;
-            } else {
-                if(node.left === null) {
-                    return node.left = new Node(data);
-                } else {
-                    node = node.left;
-                }
-                // node.left === null ? node.left = new Node(data) : node = node.left;
-            }
-        }
-        // node = new Node(data);
-        // console.log(node)
-        // return;
-    }
-}
+  constructor(arr) {
+    this.root = this.buildTree(arr);
+  }
+  buildTree(arr) {
+    // console.log(arr)
+    if (arr.length === 0) {
+      return null;
+    } else {
+      arr.sort((a, b) => a - b);
+      arr = [...new Set(arr)];
+      // console.log(arr);
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
+      let mid = Math.floor(arr.length / 2);
+      let leftArr = arr.slice(0, mid);
+      let rightArr = arr.slice(mid + 1);
+      let node = new Node(
+        arr[mid],
+        this.buildTree(leftArr),
+        this.buildTree(rightArr)
+      );
+      return node;
+    }
+  }
+  insert(data) {
+    let node = this.root;
+    if (node === null) {
+      return (node = new Node(data));
+    }
+    while (node !== null) {
+      if (node.data < data) {
+        if (node.right === null) {
+          return (node.right = new Node(data));
+        } else {
+          node = node.right;
+        }
+      } else {
+        if (node.left === null) {
+          return (node.left = new Node(data));
+        } else {
+          node = node.left;
+        }
+      }
+    }
+  }
+  delete(data, node = this.root) {
     if (node === null) {
       return;
     }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    if (node.data === data) {
+      if (node.right === null && node.left === null) {
+        return (node.data = null);
+      }
+      if (node.left === null) {
+        return (node.data = node.right);
+      }
+      if (node.right === null) {
+        return (node.data = node.left);
+      }
+      let newNode = node.right;
+      while(newNode.left !== null) {
+        newNode = newNode.left;
+      }
+      node.data = newNode.data;
+      node.right = this.delete(newNode.data, node.right);
+      return node;
+    } else if (data < node.data) {
+        node.left = this.delete(data, node.left);
+        return node;
+    } else {
+        node.right = this.delete(data, node.right);
+        return node;
     }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
 
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 let newTree = new Tree(array);
-newTree.insert(322);
+newTree.delete(67);
 
 prettyPrint(newTree.root);
 console.log(newTree.root);
